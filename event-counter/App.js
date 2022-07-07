bef30e90c2d1fa640aabb642ce993d08e2c686d1
adampatey <adampatey13@gmail.com>
2022-07-07 11:38:03 +0100

    Initial commit

---
+++ event-counter/App.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import React, {useState} from 'react';
+
+export default function App() {
+  const [count, setCount] =useState(0);
+
+  const increaseCount= () => {
+    setCount(count+1);
+  }
+
+  const decreaseCount= () => {
+    if(count > 0) {
+      setCount(count-1);
+    }else{
+      Alert.alert("Error", "You cannot have less than 0 attendees!");
+    }
+    console.log(count);
+  }
+
+  console.log(count);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.countText}>{count}</Text>
+      <Button onPress={increaseCount} title='Add one attendee'/>
+      <Button onPress={decreaseCount} title='Remove one attendee'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  countText: {
+    marginBottom:30,
+    fontSize:50
+  }
+});
